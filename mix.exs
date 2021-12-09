b20b1915d8b9862ca40a3f68fe7ebb8aabe539c0
James Harton <james@harton.nz>
2021-12-09 16:23:05 +1300

    chore(deps): update dependency git_ops to ~> 2.4

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Ease.Mixfile do
       {:ex_doc, ">= 0.26.0", only: ~w[dev test]a},
       {:earmark, ">= 0.2.1", only: ~w[dev test]a},
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
-      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false}
+      {:git_ops, "~> 2.4", only: ~w[dev test]a, runtime: false}
     ]
   end
 end
